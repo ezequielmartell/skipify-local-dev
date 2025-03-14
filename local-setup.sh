8a96d14fdf54d93af922a1799cef765f05cc0889
ezequielmartell <ezequiel@thinkmartell.com>
2025-03-13 22:10:08 -0500

    continue to improve the local development setup

--- local-setup.sh
+++ local-setup.sh
@@ -4,8 +4,17 @@
 # It will install all the necessary dependencies to run the Skipify app locally.
 
 # Download the repositories from repos.txt
-cd ..
+pushd ..
 while read repo; do
   git clone $repo
 done < repos.txt
 
+popd
+pushd config/
+mkdir -p certs
+
+# Create a self-signed SSL certificate for local development
+# This is necessary for the local server to run with HTTPS
+openssl req -x509 -newkey rsa:4096 -sha256 -days 3650 \
+  -nodes -keyout certs/spotify.local.key -out certs/spotify.local.crt -subj "/CN=spotify.local" \
+  -addext "subjectAltName=DNS:spotify.local"
